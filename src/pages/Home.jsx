9430d4c8327b10ddc197d1ccd5daa4535e01becc
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-04-14 10:44:50 -0300

    feat: Single Page with Sections en el Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import Hero from '../components/Hero';
+import AboutUs from './AboutUs';
+import Collection from './Collection';
+import Premium from './Premium';
 
 const Home = () => {
   return (
     <div className="home-page">
       <Hero />
+      <Collection />
+      <Premium />
+      <AboutUs />
     </div>
   );
 };
